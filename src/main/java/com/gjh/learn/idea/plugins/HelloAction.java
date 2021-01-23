78c99431dfc6f66a0daa08c931fe262d48958d79
guangjh <guangjinhai@163.com>
2021-01-24 00:28:58 +0800

    add tool menu group and sub menus

--- src/main/java/com/gjh/learn/idea/plugins/HelloAction.java
+++ src/main/java/com/gjh/learn/idea/plugins/HelloAction.java
@@ -1,12 +1,12 @@
 package com.gjh.learn.idea.plugins;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.ide.CopyPasteManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.codeStyle.CodeStyleManager;
 
 /**
  * created on 2021/1/23
@@ -22,5 +22,11 @@ public class HelloAction extends AnAction {
         String classPath = psiFile.getVirtualFile().getPath();
         String title = "Hello World";
         Messages.showMessageDialog(project, classPath, title, Messages.getInformationIcon());
+
+        PsiElement psiElement = e.getData(LangDataKeys.PSI_ELEMENT);
+        CodeStyleManager manager = CodeStyleManager.getInstance(psiElement.getProject());
+        manager.reformat(psiElement);
+
+        // CopyPasteManager.getInstance().setContents();
     }
 }
---
+++ src/main/java/com/gjh/learn/idea/plugins/SubToolAction.java
@@ -0,0 +1,24 @@
+package com.gjh.learn.idea.plugins;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
+
+/**
+ * created on 2021/1/23
+ *
+ * @author kevinlights
+ */
+public class SubToolAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        Messages.showMessageDialog(project, "Content", "Title", Messages.getInformationIcon());
+    }
+}
